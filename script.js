7f78d2c01afb5aaef81e1566218014f4d53a801f
Thedark2768 <109007983+Thedark2768@users.noreply.github.com>
2024-01-06 01:59:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,140 +1,168 @@
-// Elementos del DOM
+// DOM Elements
 const elements = {
-  setting: document.getElementById("svg__settings"),
-  lupa: document.getElementById("svg__lupa"),
-  subtitle: document.getElementById("subtitulo"),
-  searchInput: document.getElementById("searchInput"), // Input donde se ingresa la palabra a buscar
-  palabra: document.getElementById("palabra"),
-  word: document.getElementById("word"), // Elemento donde se muestra la palabra buscada
-  category: document.getElementById("category"), // Elemento donde se muestra la categoría de la palabra
-  definition: document.getElementById("definition"), // Elemento donde se muestra la definición de la palabra
-  example: document.getElementById("example"), // Elemento donde se muestra un ejemplo del uso de la palabra
-  errorMessage: document.getElementById("error__message"), // Elemento para mostrar mensajes de error al usuario
+  word: document.getElementById("word"),
+  savedDiv: document.getElementById("saved__div"),
+  settings: document.getElementById("svg__settings"),
+  magnifier: document.getElementById("svg__lens"),
+  subtitle: document.getElementById("caption"),
+  searchInput: document.getElementById("searchInput"),
+  frase: document.getElementById("frase"),
+  category: document.getElementById("category"),
+  definition: document.getElementById("definition"),
+  example: document.getElementById("example"),
+  errorMessage: document.getElementById("error__message"),
 };
 
-// Diccionario de palabras y sus detalles
-let diccionario = {
+// Dictionary of words and details
+let dictionary = {
   "sest": {
-    palabra: "Sest.",
-    definicion: "Sest es el acto de comer alimentos para satisfacer las necesidades nutricionales del cuerpo humano. Es un proceso mediante el cual se ingiere y se digiere la comida para obtener energía y los nutrientes necesarios.",
-    ejemplo: "Posatɾon tɾeststok ate (I’ve an attribute.).",
-    categoria: "V."
+    word: "Sest.",
+    definition: "Sest is the act of eating food to meet the nutritional needs of the human body. It is a process of ingesting and digesting food to obtain energy and necessary nutrients.",
+    example: "Posatɾon tɾeststok ate (I’ve an attribute.).",
+    category: "V."
   },
   "soet": {
-    palabra: "Soet.",
-    definicion: "Nombre oficial de la lengua construida.",
-    ejemplo: "Der tresk soet.",
-    categoria: "Sust."
+    word: "Soet.",
+    definition: "Official name of the constructed language.",
+    example: "Der tresk soet.",
+    category: "Noun"
   },
 };
 
-// Evento que maneja la entrada del usuario
+// User input event handling
 elements.searchInput.addEventListener('input', handleInput);
 
-// Función para filtrar caracteres no deseados
+// Function to filter unwanted characters
 function handleInput(event) {
   const inputValue = event.target.value;
   const filteredValue = inputValue.replace(/[^aAeEoOtTsSpPkKrRɾ]/g, '');
 
   if (inputValue !== filteredValue) {
-    // Si se ingresan caracteres no permitidos, se activa la animación de error
-    animacionError("* El carácter ingresado no es aceptado en este idioma.");
+    // Activate error animation if invalid characters are entered
+      elements.frase.style.display = "none";
+    showError("* The entered character is not accepted in this language.");
   }
 
-  // Se actualiza el valor del input con los caracteres permitidos
+  // Update input value with allowed characters
   event.target.value = filteredValue;
 }
 
-// Función para mostrar mensajes de error al usuario
-function animacionError(mensaje) {
-  // Se limpian los campos de palabra, categoría, definición y ejemplo
-  elements.palabra.style.display = "none";
+// Function to display error messages to the user
+function showError(message) {
+  // Clear word, category, definition, and example fields
   elements.word.innerText = "";
   elements.category.innerText = "";
   elements.definition.innerText = "";
   elements.example.innerText = "";
 
-  if (mensaje !== "none") {
-    // Si el mensaje no es 'none', se muestra en el área de mensajes de error
-    elements.errorMessage.innerText = mensaje;
+  if (message !== "none") {
+    // Show the error message in the error area
+    elements.errorMessage.innerText = message;
   } else {
-    // Si el mensaje es 'none', se limpia el área de mensajes de error
+    // Clear the error area
     elements.errorMessage.innerText = "";
   }
-  
+
   elements.searchInput.focus();
 }
 
-function obtenerPalabraAleatoria(diccionario) {
-  const keys = Object.keys(diccionario);
+function getRandomWord(dictionary) {
+  const keys = Object.keys(dictionary);
   const randomKey = keys[Math.floor(Math.random() * keys.length)];
-  return diccionario[randomKey];
+  return dictionary[randomKey];
 }
 
-function definirPalabra(aleatorio, palabra = null) {
-  if (!aleatorio && palabra !== null && diccionario[palabra]) {
-    const wordm = diccionario[palabra];
-    mostrarPalabra(wordm);
-  } else if (aleatorio) {
-    const palabraAleatoria = obtenerPalabraAleatoria(diccionario);
-    mostrarPalabra(palabraAleatoria);
+function defineWord(random, word = null) {
+  if (!random && word !== null && dictionary[word]) {
+    const selectedWord = dictionary[word];
+    showWord(selectedWord);
+  } else if (random) {
+    const randomWord = getRandomWord(dictionary);
+    showWord(randomWord);
   }
 }
 
-function mostrarPalabra(wordm) {
-  // Aquí debes manipular la interfaz para mostrar los valores del objeto 'word'
-  elements.word.innerText = wordm.palabra;
-  elements.definition.innerText = wordm.definicion;
-  elements.example.innerText = wordm.ejemplo;
-  elements.category.innerText = wordm.categoria;
+function showWord(selectedWord) {
+  elements.word.innerText = selectedWord.word;
+  elements.definition.innerText = selectedWord.definition;
+  elements.example.innerText = selectedWord.example;
+  elements.category.innerText = selectedWord.category;
 }
 
-// Función que se activa al presionar el botón de búsqueda
-function buscar(event) {
+// Function triggered when the search button is pressed
+function search(event) {
   const searchInput = elements.searchInput;
-  if (searchInput.getAttribute("placeholder") === "Aleatorio") {
-    elements.palabra.style.display = "block";
-    definirPalabra(true);
-  } else if (
-    (event.type === "click" || (event.type === "keypress" && event.keyCode === 13)) &&
-    searchInput.value !== ''
-  ) {
-    const palabraBuscada = searchInput.value.toLowerCase();
-    if (diccionario[palabraBuscada]) {
-      elements.palabra.style.display = "block";
-      definirPalabra(false, palabraBuscada);
+  if (searchInput.getAttribute("placeholder") === "Random") {
+    elements.frase.style.display = "block";
+    defineWord(true);
+  } else if ((event.type === "click" || (event.type === "keypress" && event.keyCode === 13)) && searchInput.value !== '') {
+    const searchedWord = searchInput.value.toLowerCase();
+    if (dictionary[searchedWord]) {
+      elements.frase.style.display = "block";
+      defineWord(false, searchedWord);
       elements.errorMessage.innerText = "";
       searchInput.blur();
-      // Aquí puedes agregar la lógica para mostrar la palabra buscada en la interfaz
     } else {
-      animacionError("* Palabra no reconocida en el diccionario.");
-      // Puedes manejar el mensaje de error en la interfaz aquí
+      showError("* Word not recognized in the dictionary.");
     }
   } else {
-    animacionError("* Escriba la palabra a definir, por favor.");
-    // Puedes manejar el mensaje de error en la interfaz aquí
+    showError("* Please enter the word to define.");
   }
 }
 
-function cambiarModo() {
-  const placeholderValue = (elements.searchInput.getAttribute("placeholder") === "Buscar") ? "Aleatorio" : "Buscar";
-  const isAleatorio = (placeholderValue === "Aleatorio");
-  
-  elements.searchInput.disabled = isAleatorio;
+function toggleMode() {
+  const placeholderValue = (elements.searchInput.getAttribute("placeholder") === "Search") ? "Random" : "Search";
+  const isRandom = (placeholderValue === "Random");
+
+  // Save the state in localStorage
+  if (isRandom === true) {
+    localStorage.setItem("randomMode", isRandom);
+  }
+
+  elements.searchInput.disabled = isRandom;
   elements.searchInput.setAttribute("placeholder", placeholderValue);
   elements.searchInput.value = "";
-  elements.palabra.style.display = "none";
+  elements.frase.style.display = "none";
 
   [elements.word, elements.category, elements.definition, elements.example, elements.errorMessage].forEach(element => element.innerText = "");
 
-  const backgroundColorBody = isAleatorio ? "#1C6E8C" : "#274156";
-  const backgroundColorInput = isAleatorio ? "#274156" : "#1C6E8C";
+  const backgroundColorBody = isRandom ? "#1C6E8C" : "#274156";
+  const backgroundColorInput = isRandom ? "#274156" : "#1C6E8C";
 
   document.body.style.backgroundColor = backgroundColorBody;
   document.documentElement.style.backgroundColor = backgroundColorBody;
-  elements.subtitle.style.display = isAleatorio ? "block" : "none";
-  elements.lupa.style.backgroundColor = backgroundColorInput;
-  elements.setting.style.backgroundColor = backgroundColorInput;
+  elements.subtitle.style.display = isRandom ? "block" : "none";
+  elements.magnifier.style.backgroundColor = backgroundColorInput;
+  elements.settings.style.backgroundColor = backgroundColorInput;
   elements.searchInput.style.backgroundColor = backgroundColorInput;
-  elements.palabra.style.backgroundColor =  backgroundColorInput;
+  elements.frase.style.backgroundColor =  backgroundColorInput;
 }
+
+function askRestoreConfiguration() {
+  const confirmation = confirm("Do you want to restore the previous mode?");
+
+  if (confirmation) {
+    const savedMode = localStorage.getItem("randomMode");
+
+    if (savedMode === "true") {
+      elements.savedDiv.classList.remove('show');
+      toggleMode(); // Restore random mode if it was saved
+    }
+  } else {
+    elements.savedDiv.classList.remove('show');
+  }
+}
+
+// Show button on page load if something is saved in localStorage
+window.onload = function () {
+  const savedMode = localStorage.getItem("randomMode");
+
+  if (savedMode === "true") {
+    elements.savedDiv.classList.add('show');
+
+    // Hide the div after 5 seconds
+    setTimeout(function() {
+      elements.savedDiv.classList.remove('show');
+    }, 5000); // 5000 milliseconds = 5 seconds
+  }
+};
